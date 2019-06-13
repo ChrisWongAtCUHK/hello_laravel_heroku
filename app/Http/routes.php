53d290d0f33eb4cae637254c864c4f90b215ce23
chriswongatcuhk <wc38.wong@gmail.com>
2019-06-13 19:44:46 +0800

    Laravel - Request Session store not set on request.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,5 +40,14 @@ Route::controller('test','ImplicitController');
 
 class MyClass{
     public $foo = 'bar';
- }
- Route::get('/myclass','ImplicitController@index');
+}
+
+Route::get('/myclass','ImplicitController@index');
+
+Route::get('/foo/bar','UriController@index');
+
+Route::get('/register',function() {
+    return view('register');
+});
+ 
+ Route::post('/user/register', array('uses'=>'UserRegistration@postRegister'));
